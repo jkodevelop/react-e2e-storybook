977d1930f8bdde3a3673c5b42f2f5b7654b2e3bc
jasonko-home <jasonko@sb2.ca>
2022-05-01 18:39:31 -0400

    WIP: storybook updated, experimental with webpack 5 build

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,25 +1,40 @@
 import React from 'react';
 
-import Button from '../source/js/components/common/Button.js';
+import { Button } from './Button';
 
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    selected: { control: 'boolean' },
+    backgroundColor: { control: 'color' },
   },
 };
 
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Selected = Template.bind({});
-Selected.args = {
-  selected: true,
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
   label: 'Button',
 };
 
-export const Default = Template.bind({});
-Default.args = {
-  selected: false,
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
   label: 'Button',
 };
--- stories/Header.stories.js
+++ stories/Header.stories.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-import Header from '../source/js/components/common/Header.js';
+import { Header } from './Header';
 
 export default {
   title: 'Example/Header',
   component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
 };
 
-export const Template = (args) => <Header {...args} />;
+const Template = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {
+    name: 'Jane Doe',
+  },
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
