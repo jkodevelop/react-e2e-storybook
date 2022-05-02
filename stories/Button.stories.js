8fd1fd707d725aa351901d4c60b352457f839ea1
jasonko-home <jasonko@sb2.ca>
2022-05-01 22:35:50 -0400

    restored storybook using webpack 5

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,40 +1,25 @@
 import React from 'react';
 
-import { Button } from './Button';
+import Button from '../source/js/components/common/Button.js';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    selected: { control: 'boolean' },
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
+export const Selected = Template.bind({});
+Selected.args = {
+  selected: true,
   label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
+export const Default = Template.bind({});
+Default.args = {
+  selected: false,
   label: 'Button',
 };
